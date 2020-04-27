01ab7180b6d8c46f0bac0922ce38d99e0b144799
MichelJuillard <michel.juillard@mjui.fr>
2020-04-27 16:05:30 +0200

    fix dgges exception

--- src/exceptions.jl
+++ src/exceptions.jl
@@ -1,4 +1,7 @@
 struct SingularException <: Exception end
 
-struct DggesException <: Exception end
+struct DggesException <: Exception
+    error_nbr::Int64
+end
 
+Base.showerror(io::IO, e::DggesException) = print(io, "error nbr", e.error_nbr)
