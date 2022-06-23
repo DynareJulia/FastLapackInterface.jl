6131abf185b404264a070eeca412c60b5266f4f7
RohitRathore1 <rohitrathore.imh55@gmail.com>
2022-06-23 14:02:46 +0530

    set CI for documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,36 @@
-using Documenter, FastLapackInterface
+using Documenter
+using FastLapackInterface
 
-makedocs(sitename = "MyDocumentation")
+DocMeta.setdocmeta!(FastLapackInterface, :DocTestSetup, :(using FastLapackInterface); recursive=true)
+
+# Build documentation.
+# ====================
+
+makedocs(
+    # options
+    modules = [FastLapackInterface],
+    doctest = true,
+    clean = false,
+    sitename = "FastLapackInterface.jl",
+    format = Documenter.HTML(
+        canonical = "",
+        assets = [],
+        edit_link = "main"
+    ),
+    pages = Any[
+        "Introduction" => "src/index.md",
+    ],
+    strict = true
+)
+
+# Deploy built documentation from Travis.
+# =======================================
+
+deploydocs(
+    # options
+    repo = "github.com/DynareJulia/FastLapackInterface.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing,
+    devbranch = "main"
+)
