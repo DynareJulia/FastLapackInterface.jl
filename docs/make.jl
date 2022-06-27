77d32e6b976f4aed5d2ae9983bb06316a25c2f50
RohitRathore1 <rohitrathore.imh55@gmail.com>
2022-06-27 13:49:18 +0530

    Switch to the regular version of Documenter.jl

--- docs/make.jl
+++ docs/make.jl
@@ -27,7 +27,7 @@ Pkg.activate(@__DIR__)
 # if !isfile(joinpath(@__DIR__, "Manifest.toml"))
 Pkg.develop(Pkg.PackageSpec(; path = ROOTPATH))
 Pkg.instantiate()
-Pkg.add(; url = "https://github.com/kimikage/Documenter.jl", rev = "ansicolor")
+Pkg.add(; url = "https://github.com/JuliaDocs/Documenter.jl")
 # end
 
 # Setup environment for making plots
