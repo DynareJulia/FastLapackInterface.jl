919765695e90fe06b6aa62c694eaf14992ffdb75
Unknown <louisponet@gmail.com>
2022-06-30 23:38:37 +0200

    changed to use geevx!

--- src/eigen.jl
+++ src/eigen.jl
@@ -1,6 +1,6 @@
 # Workspaces for Eigenvalue decomposition
 using LinearAlgebra.LAPACK: chkfinite
-import LinearAlgebra.LAPACK: geev!
+import LinearAlgebra.LAPACK: geevx!
 
 # Default with geevx! for normal, sygvd! for complex hermitian, syevr! for real hermitian
 # The assumption is to always allocate VL and VR so that eigvecs can also be calculated
@@ -12,45 +12,125 @@ struct EigenWs{T, MT <: AbstractMatrix{T}, RT<:AbstractFloat}
     VL::MT
     VR::MT
     W::Vector{T} # Can be W or WR
+    scale::Vector{RT}
+    iwork::Vector{BlasInt}
+    rconde::Vector{RT}
+    rcondv::Vector{RT}
 end
 
 function Base.show(io::IO, mime::MIME{Symbol("text/plain")}, ws::EigenWs)
     summary(io, ws)
     println(io)
-    print(io, "work: ")
-    summary(io, ws.work)
-    println(io)
-    print(io, "rwork: ")
-    summary(io, ws.rwork)
-    println(io)
-    print(io, "VL: ")
-    summary(io, ws.VL)
-    print(io, "VR: ")
-    summary(io, ws.VR)
-    print(io, "W: ")
-    return summary(io, ws.W)
+    for f in fieldnames(EigenWs)
+        print(io, "$f: ")
+        summary(io, getfield(ws, f))
+    end
 end
 
+# fns = fieldnames(EigenWs)
+# @eval Base.iterate(ws::EigenWs) = (getfield(ws, $(fns[1])), Val(fns[2]))
+# for i in 2:length(fns) - 1
+#     f = fns[i]
+#     @eval Base.iterate(ws::EigenWs, ::Val{$f}) = (getfield(ws, $f), Val(:($(fns[i+1]))))
+# end
+# @eval Base.iterate(ws::EigenWs, ::Val{$(fns[end])}) = (getfield(ws, $(fns[end])), Val(:done))
+# Base.iterate(::EigenWs, ::Val{:done})    = nothing
 Base.iterate(ws::EigenWs)                = (ws.work, Val(:rwork))
 Base.iterate(ws::EigenWs, ::Val{:rwork}) = (ws.rwork, Val(:VL))
 Base.iterate(ws::EigenWs, ::Val{:VL})    = (ws.VL, Val(:VR))
 Base.iterate(ws::EigenWs, ::Val{:VR})    = (ws.VR, Val(:W))
-Base.iterate(ws::EigenWs, ::Val{:W})     = (ws.W, Val(:done))
+Base.iterate(ws::EigenWs, ::Val{:W})     = (ws.W, Val(:scale))
+Base.iterate(ws::EigenWs, ::Val{:scale}) = (ws.scale, Val(:iwork))
+Base.iterate(ws::EigenWs, ::Val{:iwork}) = (ws.iwork, Val(:rconde))
+Base.iterate(ws::EigenWs, ::Val{:rconde}) = (ws.rconde, Val(:rcondv))
+Base.iterate(ws::EigenWs, ::Val{:rcondv}) = (ws.rcondv, Val(:done))
 Base.iterate(::EigenWs, ::Val{:done})    = nothing
 
 # (GE) general matrices eigenvalue-eigenvector and singular value decompositions
-for (geev, elty, relty) in
-    ((:dgeev_,:Float64,:Float64),
-     (:sgeev_,:Float32,:Float32),
-     (:zgeev_,:ComplexF64,:Float64),
-     (:cgeev_,:ComplexF32,:Float32))
+for (geevx, elty, relty) in
+    ((:dgeevx_,:Float64,:Float64),
+     (:sgeevx_,:Float32,:Float32),
+     (:zgeevx_,:ComplexF64,:Float64),
+     (:cgeevx_,:ComplexF32,:Float32))
     @eval begin
-        function EigenWs(A::AbstractMatrix{$elty}; lvecs = false, rvecs = true)
+        # function EigenWs(A::AbstractMatrix{$elty}; lvecs = false, rvecs = true)
+        #     chkstride1(A)
+        #     n = checksquare(A)
+        #     chkfinite(A) # balancing routines don't support NaNs and Infs
+        #     VL    = similar(A, $elty, (n, lvecs ? n : 0))
+        #     VR    = similar(A, $elty, (n, rvecs ? n : 0))
+        #     cmplx = eltype(A) <: Complex
+        #     if cmplx
+        #         W     = similar(A, $elty, n)
+        #         rwork = similar(A, $relty, 2n)
+        #     else
+        #         W     = similar(A, $elty, n)
+        #         rwork = similar(A, $elty, n)
+        #     end
+        #     work  = Vector{$elty}(undef, 1)
+        #     lwork = BlasInt(-1)
+        #     info  = Ref{BlasInt}()
+        #     jobvl = lvecs ? 'V' : 'N'
+        #     jobvr = rvecs ? 'V' : 'N'
+        #     if cmplx
+        #         ccall((@blasfunc($geev), liblapack), Cvoid,
+        #               (Ref{UInt8}, Ref{UInt8}, Ref{BlasInt}, Ptr{$elty},
+        #                Ref{BlasInt}, Ptr{$elty}, Ptr{$elty}, Ref{BlasInt},
+        #                Ptr{$elty}, Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt},
+        #                Ptr{$relty}, Ptr{BlasInt}, Clong, Clong),
+        #               jobvl, jobvr, n, A, max(1,stride(A,2)), W, VL, n, VR, n,
+        #               work, lwork, rwork, info, 1, 1)
+        #     else
+        #         ccall((@blasfunc($geev), liblapack), Cvoid,
+        #               (Ref{UInt8}, Ref{UInt8}, Ref{BlasInt}, Ptr{$elty},
+        #                Ref{BlasInt}, Ptr{$elty}, Ptr{$elty}, Ptr{$elty},
+        #                Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt}, Ptr{$elty},
+        #                Ref{BlasInt}, Ptr{BlasInt}, Clong, Clong),
+        #               jobvl, jobvr, n, A, max(1,stride(A,2)), W, rwork, VL, n,
+        #               VR, n, work, lwork, info, 1, 1)
+        #     end
+        #     chklapackerror(info[])
+        #     resize!(work, BlasInt(real(work[1])))
+        #     return EigenWs(work, rwork, VL, VR, W)
+        # end
+            
+        # function geev!(jobvl::AbstractChar, jobvr::AbstractChar, A::AbstractMatrix{$elty}, ws::EigenWs{$elty})
+        #     chkstride1(A)
+        #     n = checksquare(A)
+        #     chkfinite(A) # balancing routines don't support NaNs and Infs
+        #     work, rwork, VL, VR, W = ws
+        #     @assert jobvl != 'V' || size(VL, 2) > 0 "Workspace was created without support for left eigenvectors,\nrecreate with EigenWs(A, lvecs = true)"
+        #     @assert jobvr != 'V' || size(VR, 2) > 0 "Workspace was created without support for right eigenvectors,\nrecreate with EigenWs(A, rvecs = true)"
+               
+        #     cmplx = eltype(A) <: Complex
+        #     info  = Ref{BlasInt}()
+        #     if cmplx
+        #         ccall((@blasfunc($geev), liblapack), Cvoid,
+        #               (Ref{UInt8}, Ref{UInt8}, Ref{BlasInt}, Ptr{$elty},
+        #                Ref{BlasInt}, Ptr{$elty}, Ptr{$elty}, Ref{BlasInt},
+        #                Ptr{$elty}, Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt},
+        #                Ptr{$relty}, Ptr{BlasInt}, Clong, Clong),
+        #               jobvl, jobvr, n, A, max(1,stride(A,2)), W, VL, n, VR, n,
+        #               work, length(work), rwork, info, 1, 1)
+        #     else
+        #         ccall((@blasfunc($geev), liblapack), Cvoid,
+        #               (Ref{UInt8}, Ref{UInt8}, Ref{BlasInt}, Ptr{$elty},
+        #                Ref{BlasInt}, Ptr{$elty}, Ptr{$elty}, Ptr{$elty},
+        #                Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt}, Ptr{$elty},
+        #                Ref{BlasInt}, Ptr{BlasInt}, Clong, Clong),
+        #               jobvl, jobvr, n, A, max(1,stride(A,2)), W, rwork, VL, n,
+        #               VR, n, work, length(work), info, 1, 1)
+        #     end
+        #     chklapackerror(info[])
+        #     return cmplx ? (W, VL, VR) : (W, rwork, VL, VR)
+        # end
+        
+        function EigenWs(A::AbstractMatrix{$elty}; lvecs = false, rvecs = true, sense = false)
             chkstride1(A)
             n = checksquare(A)
             chkfinite(A) # balancing routines don't support NaNs and Infs
-            VL    = similar(A, $elty, (n, lvecs ? n : 0))
-            VR    = similar(A, $elty, (n, rvecs ? n : 0))
+            VL    = similar(A, $elty, (lvecs ? n : 0, n))
+            VR    = similar(A, $elty, (rvecs ? n : 0, n))
             cmplx = eltype(A) <: Complex
             if cmplx
                 W     = similar(A, $elty, n)
@@ -64,58 +144,115 @@ for (geev, elty, relty) in
             info  = Ref{BlasInt}()
             jobvl = lvecs ? 'V' : 'N'
             jobvr = rvecs ? 'V' : 'N'
+            
+            ilo = Ref{BlasInt}()
+            ihi = Ref{BlasInt}()
+            scale = zeros($relty, n)
+            abnrm = Ref{$relty}()
+            S = sense ? 'B' : 'N'
+            
+            rconde = zeros($relty, sense ? n : 0)
+            rcondv = zeros($relty, sense ? n : 0)
+
+            # Only needed for Float but whatever
+            iwork = zeros(BlasInt, sense ? 2n - 1 : 0)
+            
             if cmplx
-                ccall((@blasfunc($geev), liblapack), Cvoid,
-                      (Ref{UInt8}, Ref{UInt8}, Ref{BlasInt}, Ptr{$elty},
-                       Ref{BlasInt}, Ptr{$elty}, Ptr{$elty}, Ref{BlasInt},
-                       Ptr{$elty}, Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt},
-                       Ptr{$relty}, Ptr{BlasInt}, Clong, Clong),
-                      jobvl, jobvr, n, A, max(1,stride(A,2)), W, VL, n, VR, n,
-                      work, lwork, rwork, info, 1, 1)
-            else
-                ccall((@blasfunc($geev), liblapack), Cvoid,
-                      (Ref{UInt8}, Ref{UInt8}, Ref{BlasInt}, Ptr{$elty},
-                       Ref{BlasInt}, Ptr{$elty}, Ptr{$elty}, Ptr{$elty},
+                      
+                ccall((@blasfunc($geevx), liblapack), Cvoid,
+                      (Ref{UInt8}, Ref{UInt8}, Ref{UInt8}, Ref{UInt8},
                        Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt}, Ptr{$elty},
-                       Ref{BlasInt}, Ptr{BlasInt}, Clong, Clong),
-                      jobvl, jobvr, n, A, max(1,stride(A,2)), W, rwork, VL, n,
-                      VR, n, work, lwork, info, 1, 1)
+                       Ptr{$elty}, Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt},
+                       Ptr{BlasInt}, Ptr{BlasInt}, Ptr{$relty}, Ptr{$relty},
+                       Ptr{$relty}, Ptr{$relty}, Ptr{$elty}, Ref{BlasInt},
+                       Ptr{$relty}, Ptr{BlasInt}, Clong, Clong, Clong, Clong),
+                       'N', jobvl, jobvr, S,
+                       n, A, max(1,stride(A,2)), W,
+                       VL,n, VR, n,
+                       ilo, ihi, scale, abnrm,
+                       rconde, rcondv, work, lwork,
+                       rwork, info, 1, 1, 1, 1)
+                       
+            else
+                ccall((@blasfunc($geevx), liblapack), Cvoid,
+                      (Ref{UInt8}, Ref{UInt8}, Ref{UInt8}, Ref{UInt8},
+                       Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt}, Ptr{$elty},
+                       Ptr{$elty}, Ptr{$elty}, Ref{BlasInt}, Ptr{$elty},
+                       Ref{BlasInt}, Ptr{BlasInt}, Ptr{BlasInt}, Ptr{$elty},
+                       Ptr{$elty}, Ptr{$elty}, Ptr{$elty}, Ptr{$elty},
+                       Ref{BlasInt}, Ptr{BlasInt}, Ptr{BlasInt},
+                       Clong, Clong, Clong, Clong),
+                       'N', jobvl, jobvr, 'N',
+                       n, A, max(1,stride(A,2)), W,
+                       rwork, VL, n, VR,
+                       n, ilo, ihi, scale,
+                       abnrm, rconde, rcondv, work,
+                       lwork, iwork, info,
+                       1, 1, 1, 1)
             end
             chklapackerror(info[])
             resize!(work, BlasInt(real(work[1])))
-            return EigenWs(work, rwork, VL, VR, W)
+            return EigenWs(work, rwork, VL, VR, W, scale, iwork, rconde, rcondv)
         end
-            
-        function geev!(jobvl::AbstractChar, jobvr::AbstractChar, A::AbstractMatrix{$elty}, ws::EigenWs{$elty})
-            chkstride1(A)
+        
+        function geevx!(balanc::AbstractChar, jobvl::AbstractChar, jobvr::AbstractChar, sense::AbstractChar, A::AbstractMatrix{$elty}, ws::EigenWs)
             n = checksquare(A)
             chkfinite(A) # balancing routines don't support NaNs and Infs
-            work, rwork, VL, VR, W = ws
-            @assert jobvl != 'V' || size(VL, 2) > 0 "Workspace was created without support for left eigenvectors,\nrecreate with EigenWs(A, lvecs = true)"
-            @assert jobvr != 'V' || size(VR, 2) > 0 "Workspace was created without support for right eigenvectors,\nrecreate with EigenWs(A, rvecs = true)"
-               
-            cmplx = eltype(A) <: Complex
-            info  = Ref{BlasInt}()
-            if cmplx
-                ccall((@blasfunc($geev), liblapack), Cvoid,
-                      (Ref{UInt8}, Ref{UInt8}, Ref{BlasInt}, Ptr{$elty},
-                       Ref{BlasInt}, Ptr{$elty}, Ptr{$elty}, Ref{BlasInt},
-                       Ptr{$elty}, Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt},
-                       Ptr{$relty}, Ptr{BlasInt}, Clong, Clong),
-                      jobvl, jobvr, n, A, max(1,stride(A,2)), W, VL, n, VR, n,
-                      work, length(work), rwork, info, 1, 1)
-            else
-                ccall((@blasfunc($geev), liblapack), Cvoid,
-                      (Ref{UInt8}, Ref{UInt8}, Ref{BlasInt}, Ptr{$elty},
-                       Ref{BlasInt}, Ptr{$elty}, Ptr{$elty}, Ptr{$elty},
-                       Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt}, Ptr{$elty},
-                       Ref{BlasInt}, Ptr{BlasInt}, Clong, Clong),
-                      jobvl, jobvr, n, A, max(1,stride(A,2)), W, rwork, VL, n,
-                      VR, n, work, length(work), info, 1, 1)
+            if balanc ∉ ['N', 'P', 'S', 'B']
+                throw(ArgumentError("balanc must be 'N', 'P', 'S', or 'B', but $balanc was passed"))
             end
-            chklapackerror(info[])
-            return cmplx ? (W, VL, VR) : (W, rwork, VL, VR)
+            if sense ∉ ['N','E','V','B']
+                throw(ArgumentError("sense must be 'N', 'E', 'V' or 'B', but $sense was passed"))
+            end
+            work, rwork, VL, VR, W, scale, iwork, rconde, rcondv = ws
+            ldvl = size(VL, 1)
+            ldvr = size(VR, 1)
+            
+            @assert jobvl != 'V' || ldvl > 0 "Workspace was created without support for left eigenvectors,\nrecreate with EigenWs(A, lvecs = true)"
+            @assert jobvr != 'V' || ldvr > 0 "Workspace was created without support for right eigenvectors,\nrecreate with EigenWs(A, rvecs = true)"
+            
+            abnrm = Ref{$relty}()
+            ilo = Ref{BlasInt}()
+            ihi = Ref{BlasInt}()
+            info = Ref{BlasInt}()
+            cmplx = eltype(A) <: Complex
+            if cmplx
+                ccall((@blasfunc($geevx), liblapack), Cvoid,
+                      (Ref{UInt8}, Ref{UInt8}, Ref{UInt8}, Ref{UInt8},
+                       Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt}, Ptr{$elty},
+                       Ptr{$elty}, Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt},
+                       Ptr{BlasInt}, Ptr{BlasInt}, Ptr{$relty}, Ptr{$relty},
+                       Ptr{$relty}, Ptr{$relty}, Ptr{$elty}, Ref{BlasInt},
+                       Ptr{$relty}, Ptr{BlasInt}, Clong, Clong, Clong, Clong),
+                       balanc, jobvl, jobvr, sense,
+                       n, A, max(1,stride(A,2)), W,
+                       VL, max(1,ldvl), VR, max(1,ldvr),
+                       ilo, ihi, scale, abnrm,
+                       rconde, rcondv, work, length(work),
+                       rwork, info, 1, 1, 1, 1)
+                chklapackerror(info[])
+                A, W, VL, VR, ilo[], ihi[], scale, abnrm[], rconde, rcondv
+            else
+                ccall((@blasfunc($geevx), liblapack), Cvoid,
+                    (Ref{UInt8}, Ref{UInt8}, Ref{UInt8}, Ref{UInt8},
+                     Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt}, Ptr{$elty},
+                     Ptr{$elty}, Ptr{$elty}, Ref{BlasInt}, Ptr{$elty},
+                     Ref{BlasInt}, Ptr{BlasInt}, Ptr{BlasInt}, Ptr{$elty},
+                     Ptr{$elty}, Ptr{$elty}, Ptr{$elty}, Ptr{$elty},
+                     Ref{BlasInt}, Ptr{BlasInt}, Ptr{BlasInt},
+                     Clong, Clong, Clong, Clong),
+                     balanc, jobvl, jobvr, sense,
+                     n, A, max(1, stride(A,2)), W,
+                     rwork, VL, max(1,ldvl), VR,
+                     max(1,ldvr), ilo, ihi, scale,
+                     abnrm, rconde, rcondv, work,
+                     length(work), iwork, info,
+                     1, 1, 1, 1)
+                chklapackerror(info[])
+                A, W, rwork, VL, VR, ilo[], ihi[], scale, abnrm[], rconde, rcondv
+            end
         end
+            
     end
 end
 
--- test/eigen_test.jl
+++ test/eigen_test.jl
@@ -6,26 +6,40 @@ using LinearAlgebra.LAPACK
     n = 3
     @testset "Real, square" begin
         A0 = randn(n, n)
-        ws = EigenWs(copy(A0))
+        ws = EigenWs(copy(A0), lvecs=true, sense=true)
 
-        A1, t1, s1, x1 = LAPACK.geev!('N', 'V', copy(A0))
-        A2, t2, s2, x2 = LAPACK.geev!('N', 'V', copy(A0), ws)
+        A1, WR1, WI1, VL1, VR1, ilo1, ihi1, scale1, abnrm1, rconde1, rcondv1 = LAPACK.geevx!('N', 'V', 'V', 'V', copy(A0))
+        A2, WR2, WI2, VL2, VR2, ilo2, ihi2, scale2, abnrm2, rconde2, rcondv2 = LAPACK.geevx!('N', 'V', 'V', 'V', copy(A0), ws)
         @test isapprox(A1, A2)
-        @test isapprox(t1, t2)
-        @test isapprox(s1, s2)
-        @test isapprox(x1, x2)
+        @test isapprox(WR1, WR2)
+        @test isapprox(WI1, WI2)
+        @test isapprox(VL1, VL2)
+        @test isapprox(VR1, VR2)
+        @test isapprox(ilo1, ilo2)
+        @test isapprox(ihi1, ihi2)
+        @test isapprox(scale1, scale2)
+        @test isapprox(abnrm1, abnrm2)
+        @test isapprox(rconde1, rconde2, atol=1e-16)
+        @test isapprox(rcondv1, rcondv2, atol=1e-16)
         show(devnull, "text/plain", ws)
     end
 
     @testset "Complex, square" begin
         A0 = randn(ComplexF64, n, n)
-        ws = EigenWs(copy(A0))
+        ws = EigenWs(copy(A0), lvecs=true, sense=true)
 
-        W1, vs1, wr1 = LAPACK.geev!('N', 'V', copy(A0))
-        W2, vs2, wr2 = LAPACK.geev!('N', 'V', copy(A0), ws)
+        A1, W1, VL1, VR1, ilo1, ihi1, scale1, abnrm1, rconde1, rcondv1 = LAPACK.geevx!('N', 'V', 'V', 'V', copy(A0))
+        A2, W2, VL2, VR2, ilo2, ihi2, scale2, abnrm2, rconde2, rcondv2 = LAPACK.geevx!('N', 'V', 'V', 'V', copy(A0), ws)
+        @test isapprox(A1, A2)
         @test isapprox(W1, W2)
-        @test isapprox(vs1, vs2)
-        @test isapprox(wr1, wr2)
+        @test isapprox(VL1, VL2)
+        @test isapprox(VR1, VR2)
+        @test isapprox(ilo1, ilo2)
+        @test isapprox(ihi1, ihi2)
+        @test isapprox(scale1, scale2)
+        @test isapprox(abnrm1, abnrm2)
+        @test isapprox(rconde1, rconde2, atol=1e-16)
+        @test isapprox(rcondv1, rcondv2, atol=1e-16)
         show(devnull, "text/plain", ws)
     end
 end
