166975eb0edde9a6fad49286dacd2ec08a99503f
louisponet <louisponet@gmail.com>
2022-07-10 14:13:24 +0200

    Added more extended Workspace example

--- src/workspace.jl
+++ src/workspace.jl
@@ -2,6 +2,32 @@
     Workspace(lapack_function, A)
 
 Will create the correct [`Workspace`](@ref WorkSpaces) for the target `lapack_function` and matrix `A`.
+
+# Examples
+```jldoctest
+julia> A = [1.2 2.3
+            6.2 3.3]
+2×2 Matrix{Float64}:
+ 1.2  2.3
+ 6.2  3.3
+
+julia> ws = Workspace(LAPACK.geqrt!, A)
+QRWYWs{Float64, Matrix{Float64}}
+  work: 4-element Vector{Float64}
+  T: 2×2 Matrix{Float64}
+
+
+julia> LinearAlgebra.QRCompactWY(factorize!(ws, A)...)
+QRCompactWY{Float64, Matrix{Float64}, Matrix{Float64}}
+Q factor:
+2×2 QRCompactWYQ{Float64, Matrix{Float64}, Matrix{Float64}}:
+ -0.190022  -0.98178
+ -0.98178    0.190022
+R factor:
+2×2 Matrix{Float64}:
+ -6.31506  -3.67692
+  0.0      -1.63102
+```
 """
 Workspace(::typeof(LAPACK.getrf!), A::AbstractMatrix) = LUWs(A)
 
