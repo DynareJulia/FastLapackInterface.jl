f917fd22dc06269efb3c879b11822836e2120d1f
Louis Ponet <louisponet@gmail.com>
2022-07-22 21:25:50 +0200

    cholesky segfault on CI ?

--- test/cholesky_test.jl
+++ test/cholesky_test.jl
@@ -10,24 +10,26 @@ using LinearAlgebra.LAPACK
             A = (A .+ A') ./ 2
             ws = CholeskyPivotedWs(copy(A))
             
-            A1, ipiv1, rank1, inf1 = LAPACK.pstrf!('U', copy(A), 1e-6) 
-            A2, ipiv2, rank2, inf2 = LAPACK.pstrf!(ws, 'U', copy(A), 1e-6)
+            A1, ipiv1, rank1, inf1 = LAPACK.pstrf!('U', copy(A), 1e-16) 
+            A2, ipiv2, rank2, inf2 = LAPACK.pstrf!(ws, 'U', copy(A), 1e-16)
             @test A1 == A2
             @test ipiv1 == ipiv2
             @test rank1 == rank2
 
             ws = Workspace(LAPACK.pstrf!, copy(A))
-            A1, ipiv1, rank1, inf1 = decompose!(ws, Hermitian(copy(A)), 1e-6)
+            A1, ipiv1, rank1, inf1 = decompose!(ws, Hermitian(copy(A)), 1e-16)
             @test A1 == A2
             @test ipiv1 == ipiv2
             @test rank1 == rank2
             
-            A1, ipiv1, rank1, inf1 = decompose!(ws, 'U', copy(A), 1e-6)
+            A1, ipiv1, rank1, inf1 = decompose!(ws, 'U', copy(A), 1e-16)
             @test A1 == A2
             @test ipiv1 == ipiv2
             @test rank1 == rank2
 
-            decompose!(ws,'U', rand(n+1, n+1), 1e-6)
+            A = T[1 0 0 0;0 3.1 0.1 0; 0.0 0.0 5.0 0.0; 0.1 0.0 0.3 1.0]
+            A = (A .+ A') ./ 2
+            decompose!(ws,'U', A, 1e-16)
             @test length(ws.piv) == n+1
         end
     end
