53b900d2acb039cd163e539d0bdfb03b4f0c80db
Louis Ponet <louisponet@gmail.com>
2022-07-22 21:10:59 +0200

    cholesky error

--- test/cholesky_test.jl
+++ test/cholesky_test.jl
@@ -10,24 +10,24 @@ using LinearAlgebra.LAPACK
             A = (A .+ A') ./ 2
             ws = CholeskyPivotedWs(copy(A))
             
-            A1, ipiv1, rank1, inf1 = LAPACK.pstrf!('U', copy(A), 1e-16) 
-            A2, ipiv2, rank2, inf2 = LAPACK.pstrf!(ws, 'U', copy(A), 1e-16)
+            A1, ipiv1, rank1, inf1 = LAPACK.pstrf!('U', copy(A), 1e-6) 
+            A2, ipiv2, rank2, inf2 = LAPACK.pstrf!(ws, 'U', copy(A), 1e-6)
             @test A1 == A2
             @test ipiv1 == ipiv2
             @test rank1 == rank2
 
             ws = Workspace(LAPACK.pstrf!, copy(A))
-            A1, ipiv1, rank1, inf1 = decompose!(ws, Hermitian(copy(A)), 1e-16)
+            A1, ipiv1, rank1, inf1 = decompose!(ws, Hermitian(copy(A)), 1e-6)
             @test A1 == A2
             @test ipiv1 == ipiv2
             @test rank1 == rank2
             
-            A1, ipiv1, rank1, inf1 = decompose!(ws, 'U', copy(A), 1e-16)
+            A1, ipiv1, rank1, inf1 = decompose!(ws, 'U', copy(A), 1e-6)
             @test A1 == A2
             @test ipiv1 == ipiv2
             @test rank1 == rank2
 
-            decompose!(ws,'U', rand(n+1, n+1), 1e-16)
+            decompose!(ws,'U', rand(n+1, n+1), 1e-6)
             @test length(ws.piv) == n+1
         end
     end
