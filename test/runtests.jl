53f4fa87639c2ae7eff30d3c69acbc0d76951cab
MichelJuillard <michel.juillard@mjui.fr>
2022-12-05 15:12:25 +0100

    add doctest to runtest.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
+using Documenter
 using FastLapackInterface
 using LinearAlgebra
 using Test
 
+#=
 include("lse_test.jl")
 include("lu_test.jl")
 include("schur_test.jl")
@@ -9,3 +11,6 @@ include("qr_test.jl")
 include("eigen_test.jl")
 include("bunch_kaufman_test.jl")
 include("cholesky_test.jl")
+=#
+DocMeta.setdocmeta!(FastLapackInterface, :DocTestSetup, :(using FastLapackInterface, LinearAlgebra); recursive=true)
+doctest(FastLapackInterface)
