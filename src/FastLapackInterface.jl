9a7ff9845673851a89353e43f65c0f9b84e9ad0b
MichelJuillard <michel.juillard@mjui.fr>
2023-02-15 15:04:44 +0100

    link to liblapack is different in v1.9, v1.7-v1.8, <v1.7

--- src/FastLapackInterface.jl
+++ src/FastLapackInterface.jl
@@ -4,15 +4,17 @@ import Base.strides
 
 using Base: require_one_based_indexing
 using LinearAlgebra
-using LinearAlgebra: BlasInt, BlasFloat, checksquare, chkstride1, libblastrampoline
+using LinearAlgebra: BlasInt, BlasFloat, checksquare, chkstride1
 using LinearAlgebra.BLAS: @blasfunc
 using LinearAlgebra.LAPACK: chklapackerror, chkargsok, chkstride1, chktrans, chkside
 using LinearAlgebra.LAPACK
 
 @static if VERSION < v"1.7"
     using LinearAlgebra.LAPACK: liblapack
+elseif VERSION < v"1.9"
+    const liblapack = "libblastrampoline"
 else
-    const liblapack = libblastrampoline
+    const liblapack = LinearAlgebra.libblastrampoline
 end
 
 abstract type Workspace end
