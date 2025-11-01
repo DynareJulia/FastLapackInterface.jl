a477a000bf47c681e4c4e3abd67e77968c4dedfc
MichelJuillard <michel.juillard@mjui.fr>
2025-11-01 18:19:07 +0100

    add test for MKL and AppleAccelerate

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,22 @@ using FastLapackInterface
 using LinearAlgebra
 using Test
 
-include("lse_test.jl")
-include("lu_test.jl")
-include("schur_test.jl")
-include("qr_test.jl")
-include("eigen_test.jl")
-include("bunch_kaufman_test.jl")
-include("cholesky_test.jl")
-include("svd_test.jl")
+function test_FastLapackInterface()
+    include("lse_test.jl")
+    include("lu_test.jl")
+    include("schur_test.jl")
+    include("qr_test.jl")
+    include("eigen_test.jl")
+    include("bunch_kaufman_test.jl")
+    include("cholesky_test.jl")
+    include("svd_test.jl")
+end
+
+test_FastLapackInterface()
+if Sys.islinux() || Sys.iswindows()
+    using MKL
+    test_FastLapackInterface()
+elseif Sys.isapple()
+    using AppleAccelerate
+    test_FastLapackInterface()
+end
